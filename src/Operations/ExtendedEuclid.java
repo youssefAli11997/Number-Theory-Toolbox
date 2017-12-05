a550cd23a54b44c2d563c787c1c957b8c77cd67c
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-12-06 01:02:28 +0200

    Module inverse and Extended_Eculidean

--- src/Operations/ExtendedEuclid.java
+++ src/Operations/ExtendedEuclid.java
@@ -1,4 +1,23 @@
 package Operations;
 
 public class ExtendedEuclid {
+	private static int x = 0 , y = 0 , d = 0;
+	public static void extendedEcuildean(int a , int b) {
+		if (b == 0) { x = 1; y = 0; d = a; return; }
+		extendedEcuildean(b, a%b);
+		int x1 = y ;
+		int y1 = x - (a /b) *y ;
+		x = x1;
+		y = y1;
+		return ;
+	}
+	
+	public static int moduleInverse(int a , int n) {
+		extendedEcuildean(a, n);
+		return x ;
+	}
+	
+	public static void main (String [] args) {
+		System.out.println(moduleInverse(13, 15));
+	}
 }
