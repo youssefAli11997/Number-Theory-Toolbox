b6526c09ddbf91ed7fd2dbf53415a119a98121d1
Youssef_Ali <youssefali6212@gmail.com>
2017-12-08 19:13:07 +0200

    Refactoring and Calculating Execution Time for The 4 Modular Exponentiation Methods

--- src/Algorithms/ModularExponentiation.java
+++ src/Algorithms/ModularExponentiation.java
@@ -1,6 +1,7 @@
 package Algorithms;
 
 import java.math.BigInteger;
+import java.util.Random;
 
 public class ModularExponentiation {
 
@@ -24,6 +25,7 @@ public class ModularExponentiation {
     }
 
     private BigInteger straightforward(){
+        long startTime = System.nanoTime();
         if(n.equals(BigInteger.ONE)){
             return BigInteger.ZERO;
         }
@@ -33,10 +35,13 @@ public class ModularExponentiation {
             answer = answer.multiply(a);
             i = i.add(BigInteger.ONE);
         }
+        long endTime = System.nanoTime();
+        System.out.println("SF:" + (endTime - startTime) / 1000 + " ms");
         return answer.mod(n);
     }
 
     private BigInteger memoryEfficient(){
+        long startTime = System.nanoTime();
         if(n.equals(BigInteger.ONE)){
             return BigInteger.ZERO;
         }
@@ -46,27 +51,13 @@ public class ModularExponentiation {
             answer = (answer.multiply(a)).mod(n);
             i = i.add(BigInteger.ONE);
         }
+        long endTime = System.nanoTime();
+        System.out.println("ME:" + (endTime - startTime) / 1000 + " ms");
         return answer;
     }
 
     private BigInteger rightToLeftBinary(){
-        if(n.equals(BigInteger.ONE)){
-            return BigInteger.ZERO;
-        }
-        BigInteger answer = BigInteger.ONE;
-        BigInteger newBase = a.mod(n);
-        BigInteger tempB = new BigInteger(b.toString());
-        while(tempB.max(BigInteger.ZERO).equals(tempB) && !tempB.equals(BigInteger.ZERO)){
-            if (tempB.mod(BigInteger.valueOf(2)).equals(BigInteger.ONE)) {
-                answer = (answer.multiply(newBase)).mod(n);
-            }
-            tempB = tempB.shiftRight(1);
-            newBase = (newBase.multiply(newBase)).mod(n);
-        }
-        return answer;
-    }
-
-    private BigInteger rightToLeftBinary2(){
+        long startTime = System.nanoTime();
         if(n.equals(BigInteger.ONE)){
             return BigInteger.ZERO;
         }
@@ -80,36 +71,53 @@ public class ModularExponentiation {
             tempB = tempB.shiftRight(1);
             newBase = (newBase.multiply(newBase));
         }
+        long endTime = System.nanoTime();
+        System.out.println("RL:" + (endTime - startTime) / 1000  + " ms");
         return answer.mod(n);
     }
 
-    /*private BigInteger leftToRightBinary(){
-        System.out.println("---------------\nlr");
+    private BigInteger rightToLeftBinary2(){
+        long startTime = System.nanoTime();
         if(n.equals(BigInteger.ONE)){
             return BigInteger.ZERO;
         }
         BigInteger answer = BigInteger.ONE;
         BigInteger newBase = a.mod(n);
         BigInteger tempB = new BigInteger(b.toString());
-        int bitLength = tempB.bitLength();
-        BigInteger i = BigInteger.valueOf(bitLength-2);
-        System.out.println("HHH: " + tempB.testBit(bitLength-1));
-        while(!i.equals(BigInteger.valueOf(-1))){
-            System.out.println(tempB.testBit(i.intValue()));
-            System.out.println(answer);
-            if (tempB.testBit(i.intValue())) {
+        while(tempB.max(BigInteger.ZERO).equals(tempB) && !tempB.equals(BigInteger.ZERO)){
+            if (tempB.mod(BigInteger.valueOf(2)).equals(BigInteger.ONE)) {
                 answer = (answer.multiply(newBase)).mod(n);
             }
-            i = i.subtract(BigInteger.ONE);
+            tempB = tempB.shiftRight(1);
             newBase = (newBase.multiply(newBase)).mod(n);
         }
+        long endTime = System.nanoTime();
+        System.out.println("RL2:" + (endTime - startTime) / 1000 + " ms");
         return answer;
-    }*/
+    }
+
+    private static BigInteger generateRandomBigInteger(int numOfdigits){
+        Random rand = new Random();
+        String number = "";
+        for(int i=0; i<numOfdigits; i++){
+            //System.out.println("it : " + Math.abs(rand.nextInt())%10);
+            number += (char)(Math.abs(rand.nextInt(10))%10 + '0');
+        }
+        return new BigInteger(number);
+    }
 
     public static void main(String[] args){
-        ModularExponentiation me = new ModularExponentiation("3","11","55");
-        for(int i=1; i<5; i++){
-            System.out.println(me.execute(i));
-        }
+        BigInteger a,b,n;
+        a = generateRandomBigInteger(6);
+        b = generateRandomBigInteger(6);
+        n = generateRandomBigInteger(6);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(n);
+
+        ModularExponentiation me = new ModularExponentiation(a.toString(),b.toString(),n.toString());
+        for(int i=1; i<=4; i++)
+            me.execute(i);
+        System.out.println("hrr2");
     }
 }
